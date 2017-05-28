903d8413f0af5a07273482593b2cf37491f6759d
Devin Matte <devinmatte@gmail.com>
2017-05-28 13:43:43 -0400

    Updating to now work with RIT Content

--- index.js
+++ index.js
@@ -15,50 +15,25 @@ const FACTS_CONTEXT = 'choose_fact-followup';
 const DEFAULT_LIFESPAN = 5;
 const END_LIFESPAN = 0;
 
-const FACT_TYPE = {
+const INFORMATION_TYPE = {
     HISTORY: 'history',
-    HEADQUARTERS: 'headquarters',
-    CATS: 'cats'
+    MAJORS: 'majors'
 };
 
 const HISTORY_FACTS = new Set([
-    'Google was founded in 1998.',
-    'Google was founded by Larry Page and Sergey Brin.',
-    'Google went public in 2004.',
-    'Google has more than 70 offices in more than 40 countries.'
+    'FOUNDED IN 1829, Rochester Institute of Technology is a privately endowed, coeducational university with nine colleges emphasizing career education and experiential learning.',
+    'THE CAMPUS occupies 1,300 acres in suburban Rochester, the third-largest city in New York state. RIT also has international locations in China, Croatia, Dubai, and Kosovo. ',
 ]);
 
-const HQ_FACTS = new Set([
-    'Google\'s headquarters is in Mountain View, California.',
-    'Google has over 30 cafeterias in its main campus.',
-    'Google has over 10 fitness facilities in its main campus.'
-]);
-
-const GOOGLE_IMAGES = [
+const RIT_IMAGES = [
     [
-        'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Search_GSA.2e16d0ba.fill-300x300.png',
-        'Google app logo'
-    ],
-    [
-        'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Google_Logo.max-900x900.png',
-        'Google logo'
-    ],
-    [
-        'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Dinosaur-skeleton-at-Google.max-900x900.jpg',
-        'Stan the Dinosaur at Googleplex'
-    ],
-    [
-        'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Wide-view-of-Google-campus.max-900x900.jpg',
-        'Googleplex'
-    ],
-    [
-        'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Bikes-on-the-Google-campus.2e16d0ba.fill-300x300.jpg',
-        'Biking at Googleplex'
+        'http://www.rit.edu/upub/sites/rit.edu.upub/files/logos/tiger_walking_rit_color.jpg',
+        'RIT Tiger Logo'
     ]
 ];
 
 const LINK_OUT_TEXT = 'Learn more';
-const GOOGLE_LINK = 'https://www.google.com/about/';
+const RIT_LINK = 'https://www.rit.edu/overview/rit-in-brief';
 const NEXT_FACT_DIRECTIVE = ' Would you like to hear another fact?';
 const CONFIRMATION_SUGGESTIONS = ['Sure', 'No thanks'];
 
@@ -68,12 +43,12 @@ const NO_INPUTS = [
     'We can stop here. See you soon.'
 ];
 
-function getRandomImage (images) {
+function getRandomImage(images) {
     let randomIndex = Math.floor(Math.random() * images.length);
     return images[randomIndex];
 }
 
-function getRandomFact (facts) {
+function getRandomFact(facts) {
     if (facts.size <= 0) {
         return null;
     }
@@ -93,65 +68,55 @@ function getRandomFact (facts) {
 }
 
 exports.RITAssistant = (request, response) => {
-    const app = new App({ request, response });
+    const app = new App({request, response});
     console.log('Request headers: ' + JSON.stringify(request.headers));
     console.log('Request body: ' + JSON.stringify(request.body));
 
     // Greet the user and direct them to next turn
-    function unhandledDeepLinks (app) {
+    function unhandledDeepLinks(app) {
         if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
             app.ask(app.buildRichResponse()
-                .addSimpleResponse(`Welcome to Facts about Google! I'd really rather \
-not talk about ${app.getRawInput()}. Wouldn't you rather talk about \
-Google? I can tell you about Google's history or its headquarters. \
-Which do you want to hear about?`)
-                .addSuggestions(['History', 'Headquarters']), NO_INPUTS);
+                .addSimpleResponse(`Welcome to R.I.T Assistant! I'd really rather \
+not talk about ${app.getRawInput()}. What do you want to know about R.I.T?`)
+                .addSuggestions(['Trivia', 'Majors']), NO_INPUTS);
         } else {
-            app.ask(`Welcome to Facts about Google! I'd really rather \
+            app.ask(`Welcome to R.I.T Assistant! I'd really rather \
 not talk about ${app.getRawInput()}. \
-Wouldn't you rather talk about Google? I can tell you about \
-Google's history or its headquarters. Which do you want to hear about?`,
+What do you want to know about R.I.T?`,
                 NO_INPUTS);
         }
     }
 
     // Say a fact
-    function tellFact (app) {
+    function tellFact(app) {
         let historyFacts = app.data.historyFacts
             ? new Set(app.data.historyFacts) : HISTORY_FACTS;
-        let hqFacts = app.data.hqFacts ? new Set(app.data.hqFacts) : HQ_FACTS;
-
-        if (historyFacts.size === 0 && hqFacts.size === 0) {
-            app.tell('Actually it looks like you heard it all. ' +
-                'Thanks for listening!');
-            return;
-        }
 
         let factCategory = app.getArgument(CATEGORY_ARGUMENT);
 
-        if (factCategory === FACT_TYPE.HISTORY) {
+        if (factCategory === INFORMATION_TYPE.HISTORY) {
             let fact = getRandomFact(historyFacts);
             if (fact === null) {
                 if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
-                    let suggestions = ['Headquarters'];
+                    let suggestions = ['Majors'];
                     app.ask(app.buildRichResponse()
-                        .addSimpleResponse(noFactsLeft(app, factCategory, FACT_TYPE.HEADQUARTERS))
+                        .addSimpleResponse(noFactsLeft(app, factCategory, INFORMATION_TYPE.MAJORS))
                         .addSuggestions(suggestions), NO_INPUTS);
                 } else {
-                    app.ask(noFactsLeft(app, factCategory, FACT_TYPE.HEADQUARTERS),
+                    app.ask(noFactsLeft(app, factCategory, INFORMATION_TYPE.MAJORS),
                         NO_INPUTS);
                 }
                 return;
             }
 
-            let factPrefix = 'Sure, here\'s a history fact. ';
+            let factPrefix = 'Sure, here\'s a trivia fact. ';
             app.data.historyFacts = Array.from(historyFacts);
             if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
-                let image = getRandomImage(GOOGLE_IMAGES);
+                let image = getRandomImage(RIT_IMAGES);
                 app.ask(app.buildRichResponse()
                     .addSimpleResponse(factPrefix)
                     .addBasicCard(app.buildBasicCard(fact)
-                        .addButton(LINK_OUT_TEXT, GOOGLE_LINK)
+                        .addButton(LINK_OUT_TEXT, RIT_LINK)
                         .setImage(image[0], image[1]))
                     .addSimpleResponse(NEXT_FACT_DIRECTIVE)
                     .addSuggestions(CONFIRMATION_SUGGESTIONS), NO_INPUTS);
@@ -159,59 +124,28 @@ Google's history or its headquarters. Which do you want to hear about?`,
                 app.ask(factPrefix + fact + NEXT_FACT_DIRECTIVE, NO_INPUTS);
             }
             return;
-        } else if (factCategory === FACT_TYPE.HEADQUARTERS) {
-            let fact = getRandomFact(hqFacts);
-            if (fact === null) {
-                if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
-                    let suggestions = ['History'];
-                    app.ask(app.buildRichResponse()
-                        .addSimpleResponse(noFactsLeft(app, factCategory, FACT_TYPE.HISTORY))
-                        .addSuggestions(suggestions), NO_INPUTS);
-                } else {
-                    app.ask(noFactsLeft(app, factCategory, FACT_TYPE.HISTORY), NO_INPUTS);
-                }
-                return;
-            }
+        } else if (factCategory === INFORMATION_TYPE.MAJORS) {
 
-            let factPrefix = 'Okay, here\'s a headquarters fact. ';
-            app.data.hqFacts = Array.from(hqFacts);
-            if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
-                let image = getRandomImage(GOOGLE_IMAGES);
-                app.ask(app.buildRichResponse()
-                    .addSimpleResponse(factPrefix)
-                    .addBasicCard(app.buildBasicCard(fact)
-                        .setImage(image[0], image[1])
-                        .addButton(LINK_OUT_TEXT, GOOGLE_LINK))
-                    .addSimpleResponse(NEXT_FACT_DIRECTIVE)
-                    .addSuggestions(CONFIRMATION_SUGGESTIONS), NO_INPUTS);
-            } else {
-                app.ask(factPrefix + fact + NEXT_FACT_DIRECTIVE, NO_INPUTS);
-            }
-            return;
         } else {
             // Conversation repair is handled in API.AI, but this is a safeguard
             if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
                 app.ask(app.buildRichResponse()
-                    .addSimpleResponse(`Sorry, I didn't understand. I can tell you about \
-Google's history, or its  headquarters. Which one do you want to \
-hear about?`)
-                    .addSuggestions(['History', 'Headquarters']), NO_INPUTS);
+                    .addSimpleResponse(`Sorry, I didn't understand. What do you want to know about R.I.T?`)
+                    .addSuggestions(['Trivia', 'Majors']), NO_INPUTS);
             } else {
-                app.ask(`Sorry, I didn't understand. I can tell you about \
-Google's history, or its headquarters. Which one do you want to \
-hear about?`, NO_INPUTS);
+                app.ask(`Sorry, I didn't understand. What do you want to know about R.I.T?`, NO_INPUTS);
             }
         }
     }
 
     // Say they've heard it all about this category
-    function noFactsLeft (app, currentCategory, redirectCategory) {
+    function noFactsLeft(app, currentCategory, redirectCategory) {
         let parameters = {};
         parameters[CATEGORY_ARGUMENT] = redirectCategory;
         // Replace the outgoing facts context with different parameters
         app.setContext(FACTS_CONTEXT, DEFAULT_LIFESPAN, parameters);
         let response = `Looks like you've heard all there is to know \
-about the ${currentCategory} of Google. I could tell you about its \
+about the ${currentCategory} of R.I.T. I could tell you about more specific things, like \
 ${redirectCategory} instead. `;
         response += `So what would you like to hear about?`;
         return response;
