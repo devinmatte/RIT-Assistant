87014b25267320885af0744b9e20abec36308e3c
Devin Matte <devinmatte@gmail.com>
2017-06-04 13:48:46 -0400

    Trying out new Flask Cards

--- webhook.py
+++ webhook.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask_assistant import Assistant, ask, tell
+from flask_assistant import Assistant, ask
 import random
 
 app = Flask(__name__)
+app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
 assist = Assistant(app, '/')
 
 
@@ -17,7 +18,8 @@ def input_welcome():
 def deeplink_unknown():
     speech = 'Welcome to R.I.T Assistant! I\'m not quite sure how to respond to that. ' \
              'What do you want to know about R.I.T?'
-    return ask(speech)
+    resp = ask(speech)
+    return resp
 
 
 @assist.action('Trivia')
@@ -26,13 +28,16 @@ def tell_fact():
     facts = ['R.I.T was founded in 1829.', 'R.I.T\'s president is David C. Munson.',
              'Prior to 1955, athletic teams referred to themselves as The Techmen or The Blue Grey.']
     speech = random.choice(facts) + ' What else do you want to know about?'
-    return ask(speech)
+    resp = ask(speech)
+    return resp
 
 
 @assist.action('Majors')
 def tell_majors():
     speech = 'These are Majors my dude'
-    return ask(speech)
+    resp = ask(speech)
+    resp.card(text='Majors and Stuff')
+    return resp
 
 
 if __name__ == '__main__':
